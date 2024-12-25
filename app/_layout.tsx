2dbb63bedabbfbeded36f40a15d21e97b6acaeb5
Paulinha <paulafabianasoares@gmail.com>
2024-12-24 22:49:51 -0300

    [add] gluesack-ui lib

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,8 @@ import {
   DefaultTheme,
   ThemeProvider,
 } from "@react-navigation/native";
+import "@/global.css";
+import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -31,7 +33,7 @@ export default function RootLayout() {
   }
 
   return (
-    <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+    <GluestackUIProvider mode="light">
       <Stack
         screenOptions={{
           headerShown: false,
@@ -41,6 +43,6 @@ export default function RootLayout() {
         <Stack.Screen name="login" />
         <Stack.Screen name="+not-found" />
       </Stack>
-    </ThemeProvider>
+    </GluestackUIProvider>
   );
 }
