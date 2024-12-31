0a1dde50eddccdd57ce793628e021feea9ec45c2
Paulinha <paulafabianasoares@gmail.com>
2024-12-31 17:33:45 -0300

    [clean] removed non used code - paddingHorizontal

--- app/(authenticated)/topics/az-list.tsx
+++ app/(authenticated)/topics/az-list.tsx
@@ -36,7 +36,6 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    paddingHorizontal: 15,
     backgroundColor: Colors.light.background,
   },
 });
