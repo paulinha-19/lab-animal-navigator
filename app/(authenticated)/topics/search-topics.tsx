867a20af8fc607522d3df095d66a99c069bea3ff
Paulinha <paulafabianasoares@gmail.com>
2025-02-09 20:55:44 -0300

    [update] az-list to into of SearchTopicsScreen

--- app/(authenticated)/topics/search-topics.tsx
+++ app/(authenticated)/topics/search-topics.tsx
@@ -1,29 +1,22 @@
 import React from "react";
-import { StyleSheet, Text, SafeAreaView, ScrollView } from "react-native";
-import { SearchInfo } from "@/components";
+import { StyleSheet, SafeAreaView, View } from "react-native";
 import { SearchBar } from "@/components/common/SearchBar";
 import { Header } from "@/components/common/Header";
 import { Colors } from "@/constants/Colors";
 import { Box } from "@/components/ui/box";
-import { router } from "expo-router";
 import { useSearch } from "@/hooks/useSearch";
 import { SearchResults } from "@/components/SearchResults";
+import AZListScreen from "./az-list";
 
 export default function SearchTopicsScreen() {
   const { searchQuery, filteredData, handleSearch } = useSearch();
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView style={{ flex: 1, backgroundColor: Colors.light.background }}>
+      <View style={{ flex: 1, backgroundColor: Colors.light.background }}>
         <Box className="bg-primary-500" style={styles.containerHeader}>
           <Header>
-            <SearchBar onSearch={handleSearch} searchQuery={searchQuery}/>
-            <Text
-              style={styles.azText}
-              onPress={() => router.push("/(authenticated)/topics/az-list")}
-            >
-              A-Z
-            </Text>
+            <SearchBar onSearch={handleSearch} searchQuery={searchQuery} />
           </Header>
         </Box>
 
@@ -32,10 +25,10 @@ export default function SearchTopicsScreen() {
           {searchQuery.length >= 3 ? (
             <SearchResults filteredData={filteredData} />
           ) : (
-            <SearchInfo />
+            <AZListScreen />
           )}
         </Box>
-      </ScrollView>
+      </View>
     </SafeAreaView>
   );
 }
@@ -49,6 +42,7 @@ const styles = StyleSheet.create({
     padding: 15,
     alignItems: "center",
     marginTop: 20,
+    flex: 1,
   },
   containerHeader: {
     marginTop: 5,
