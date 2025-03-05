d4fb7b2efc3d7323c3184d6d965cce7201386b98
Paulinha <paulafabianasoares@gmail.com>
2025-03-04 22:26:17 -0300

    [add] types index.ts

---
+++ types/index.ts
@@ -0,0 +1,23 @@
+import {
+  TypeCalculationAdministered,
+  CalculationAdministeredForm,
+} from "./calculations";
+import {
+  ForgotPasswordType,
+  PasswordEmail,
+  ResetType,
+  TokenType,
+  UserDataType,
+  TokenResetPasswordType
+} from "./auth-data";
+
+export {
+  TypeCalculationAdministered,
+  CalculationAdministeredForm,
+  ForgotPasswordType,
+  PasswordEmail,
+  ResetType,
+  TokenType,
+  UserDataType,
+  TokenResetPasswordType
+};
