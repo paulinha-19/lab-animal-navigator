f75bbf09b2b627f33a010977c6d81d1bb4ac136c
Paulinha <paulafabianasoares@gmail.com>
2025-03-04 22:14:44 -0300

    [add] components common index.ts

---
+++ components/common/index.ts
@@ -0,0 +1,11 @@
+import { ControlledSelect } from "./ControlledSelect";
+import { Divider } from "./Divider";
+import { Header } from "./Header";
+import { SearchBar } from "./SearchBar";
+
+export {
+    ControlledSelect,
+    Divider,
+    Header,
+    SearchBar,
+}
