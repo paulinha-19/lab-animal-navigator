719e8a489a526a7ded4cfc0369b36814cefbe6cf
Paulinha <paulafabianasoares@gmail.com>
2025-03-04 22:25:40 -0300

    [add] import zod, calculationAdministeredSchema and export type CalculationAdministeredForm

--- types/calculations.ts
+++ types/calculations.ts
@@ -1,6 +1,13 @@
+import { z } from "zod";
+import {calculationAdministeredSchema} from "../schemas/index";
+
 export type TypeCalculationAdministered = {
   selectSpecies: string;
   selectLineage: string;
   selectPhaseLife: string;
   numberAnimals: number;
 };
+
+export type CalculationAdministeredForm = z.infer<
+  typeof calculationAdministeredSchema
+>;
